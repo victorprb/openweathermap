709cd928cf5d6c818a9e0c486c1f95ae96cdc445
victorprb <victorp.ribeiro@gmail.com>
2020-02-01 19:37:43 -0300

    Create methods to pass test

--- lib/openweathermap/api.rb
+++ lib/openweathermap/api.rb
@@ -1,9 +1,37 @@
+require 'httparty'
+require 'json'
+
 module OpenWeatherMap
   class API
+    BASE_URL = 'https://api.openweathermap.org'
+
+    def initialize(config = {})
+      @options = {
+        query: {
+          id: config[:city_id],
+          appid: config[:api_key],
+          units: config[:units] || 'metric',
+          lang: config[:lang] || 'pt_br'
+        }
+      }
+    end
+
     def weather
+      @response = data('weather')
     end
 
     def forecast
+      @response = data('forecast')
+    end
+
+    private
+
+    def data(resource)
+      begin
+        HTTParty.get("#{BASE_URL}/data/2.5/#{resource}", @options)
+      rescue => e
+        "#{e.class.name} : #{e.message}"
+      end
     end
   end
 end
